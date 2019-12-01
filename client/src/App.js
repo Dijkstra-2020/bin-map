76cccb10a9cbeaf33f82c0f7871ea7fbad5986b1
Antonio Ren <antonio.ren@epita.fr>
2019-12-01 15:04:40 +0000

    [API DB] Fix url connection

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,23 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+ var url = "http://" + window.location.hostname;
+
+
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = { apiResponse: "", dbResponse: "" };
   }
   callAPI() {
-    fetch("http://localhost:9000/testAPI")
+    fetch(url+":9000/testAPI")
         .then(res => res.text())
         .then(res => this.setState({ apiResponse: res }));
   }
 
   callDB() {
-    fetch("http://localhost:9000/testDB")
+    fetch(url+":9000/testDB")
         .then(res => res.text())
         .then(res => this.setState({ dbResponse: res }))
         .catch(err => err);
@@ -30,7 +34,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React
+</h1>
         </header>
         <p className="App-intro">{this.state.apiResponse}</p>
         <p className="App-intro">{this.state.dbResponse}</p>
