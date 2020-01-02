8eac25d8b8483a6c410d21f1d310a7cffb764b0e
Antonio Ren <antonio.ren@epita.fr>
2020-01-02 11:46:00 +0000

    Update google map key

--- client/src/App.js
+++ client/src/App.js
@@ -59,5 +59,5 @@ class App extends Component {
 
 
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBn2fjl7XeGM2AwJUELskaplk_wL6hm1yY")
+  apiKey: ("AIzaSyC3iJhPLVygKr7HUMH58b4uOvfC8m5s1H0")
 })(App)
